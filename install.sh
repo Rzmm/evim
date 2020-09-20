740e6e32f9628dbe9b4a5e570b6e0ee4bc4cf049
r1024 <wenchen.mails@gmail.com>
2020-09-20 11:45:02 +0800

    feat(null): 提供正是安装脚本

--- install.sh
+++ install.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
 
-echo "eeee"
+if [ -f $HOME/.vimrc ]; then
+	backup_dot_vimrc="$HOME/.vimrc`date '+%y%m%d_%H%M%S'`"
+	echo "$HOME/.vimrc already exist, easyvim backup it to $backup_dot_vimrc"
+	cp -f $HOME/.vimrc $backup_dot_vimrc
+fi
+
+if [ -f $HOME/.vim ]; then
+	backup_dot_vim="$HOME/.vim`date '+%y%m%d_%H%M%S'`"
+	echo "$HOME/.vim already exist, easyvim backup it to $backup_dot_vim"
+	cp -rf $HOME/.vim $backup_dot_vim
+fi
+
+echo ""
+
+git clone git@github.com:r1024/easyvim.git $HOME/.vim > /dev/null 2>&1
+
+# install plugins
+mkdir ~/.vim/bundle/
+echo "Clone delimitMate ..."
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "source ~/.vim/basic.vim" > $HOME/.vimrc 
+echo "source ~/.vim/plugins.vim" >> $HOME/.vimrc
+
+echo "Easyvim install succsess!"
+echo ""
