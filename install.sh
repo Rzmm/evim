0184dd2a46c80c9dd11431538fad41c445e54743
chenwen(陈文) <66820507+r1024@users.noreply.github.com>
2021-01-08 15:59:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,3 +18,18 @@ echo "source ~/.vim/plugins.vim" >> $HOME/.vimrc
 
 echo "Easyvim install succsess, exec :PluginInstall in VIM!"
 echo ""
+
+
+在Git库中增加子模块
+git submodule add <url_of_submodule>
+git commit -am 'added <submodule>'
+
+克隆含有子模块的项目
+git clone <url_of_MainProject>
+cd MainProject/<submodule>
+git submodule init && git submodule update
+
+从子模块的远端拉取上游修改
+cd MainProject/<submodule>
+git pull
+
