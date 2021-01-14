f7451dbb262fd254294aa11f52c1c434aa4105e4
chenwen(陈文) <66820507+r1024@users.noreply.github.com>
2021-01-14 13:09:40 +0800

    fix: no ~/.vim/bundle/ error

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo ""
 
 # install Vundle
 echo "install Vundle..."
-mkdir ~/.vim/bundle/
+mkdir -p ~/.vim/bundle/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "source ~/.vim/basic.vim" > $HOME/.vimrc 
